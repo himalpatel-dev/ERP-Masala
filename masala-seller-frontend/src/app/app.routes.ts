0e86e677f90dfb8718c4927a446ec996f5b80f19
Himal Patel <hnp.2792@gmail.com>
2025-11-26 18:14:55 +0530

    frontend start

---
+++ masala-seller-frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+
+export const routes: Routes = [
+    {
+        path: 'admin',
+        component: DashboardComponent
+    }
+];
